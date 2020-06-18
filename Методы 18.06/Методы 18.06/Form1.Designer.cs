035516f704030941a71791e8fe1bac19a40c6e28
EvgenyVodyannikov <vodyannikov.eugene@gmail.com>
2020-06-18 13:44:32 +0500

    метод

--- Методы 18.06/Методы 18.06/Form1.Designer.cs
+++ Методы 18.06/Методы 18.06/Form1.Designer.cs
@@ -34,13 +34,20 @@
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(214, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(299, 6);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(413, 271);
             this.pictureBox1.TabIndex = 0;
@@ -48,25 +55,33 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox3);
             this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(12, 44);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(180, 239);
+            this.groupBox1.Size = new System.Drawing.Size(260, 271);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "Координаты 1-го треугольника";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(35, 197);
+            this.button1.Location = new System.Drawing.Point(11, 193);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(112, 36);
             this.button1.TabIndex = 0;
             this.button1.Text = "Записать";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -76,22 +91,76 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 43);
+            this.label1.Location = new System.Drawing.Point(6, 67);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 25);
+            this.label1.Size = new System.Drawing.Size(29, 25);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "А:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(75, 40);
+            this.textBox1.Location = new System.Drawing.Point(41, 64);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(30, 33);
             this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(77, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(30, 33);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "B:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(41, 104);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(30, 33);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(77, 104);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(30, 33);
+            this.textBox4.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "C:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(41, 145);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(30, 33);
+            this.textBox5.TabIndex = 2;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(77, 145);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(30, 33);
+            this.textBox6.TabIndex = 2;
             // 
             // Form1
             // 
@@ -118,6 +187,13 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Методы 18.06/Методы 18.06/Form1.cs
+++ Методы 18.06/Методы 18.06/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,60 @@ namespace Методы_18._06
         public Form1()
         {
             InitializeComponent();
+        }
+        public Point A, B, C, D, E, F;
+
+
+        static bool Proverka(Point A, Point B, Point C, Point D, out bool result) // D - точка для проверки
+        {
+            result = false;
+            if ((A.Y - B.Y) * D.X + (B.X - A.X) * D.Y + (A.X * B.Y - B.X * A.Y) < 0) // проверка лежит ли точка D за отрезком AB
+            {
+                if ((C.Y - B.Y) * D.X + (B.X - C.X) * D.Y + (C.X * B.Y - B.X * C.Y) < 0)  // проверка лежит ли точка D за отрезком BC
+                {
+                    if ((C.Y - A.Y) * D.X + (A.X - C.X) * D.Y + (C.X * A.Y - A.X * C.Y) < 0) // проверка лежит ли точка D за отрезком AC
+                    {
+                        result = true;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool resultTochka1 = false;
+            bool resultTochka2 = false;
+            bool resultTochka3 = false;
+            Proverka(A, B, C, D, out resultTochka1);
+            Proverka(A, B, C, D, out resultTochka2);
+            Proverka(A, B, C, D, out resultTochka3);
+            if(resultTochka1 && resultTochka2 && resultTochka3) { MessageBox.Show("ДА!!!"); }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(groupBox1.Text == "Координаты 1-го треугольника")
+            {
+                A.X = Int32.Parse(textBox1.Text);
+                A.Y = Int32.Parse(textBox2.Text);
+                B.X = Int32.Parse(textBox3.Text);
+                B.Y = Int32.Parse(textBox4.Text);
+                C.X = Int32.Parse(textBox5.Text);
+                C.Y = Int32.Parse(textBox6.Text);
+                groupBox1.Text = "Координаты 2-го треугольника";
+                label1.Text = "D"; label2.Text = "E"; label3.Text = "F";
+            }
+            else
+            {
+                D.X = Int32.Parse(textBox1.Text);
+                D.Y = Int32.Parse(textBox2.Text);
+                E.X = Int32.Parse(textBox3.Text);
+                E.Y = Int32.Parse(textBox4.Text);
+                F.X = Int32.Parse(textBox5.Text);
+                F.Y = Int32.Parse(textBox6.Text);
+                button1.Enabled = false;
+            }
         }
     }
 }
