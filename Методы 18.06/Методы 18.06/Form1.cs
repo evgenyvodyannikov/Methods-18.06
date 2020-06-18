f283d397de2e277f73f8b32f5a4a5689ae361e77
EvgenyVodyannikov <vodyannikov.eugene@gmail.com>
2020-06-18 13:51:26 +0500

    верный метод

--- Методы 18.06/Методы 18.06/Form1.cs
+++ Методы 18.06/Методы 18.06/Form1.cs
@@ -23,11 +23,11 @@ namespace Методы_18._06
         static bool Proverka(Point A, Point B, Point C, Point D, out bool result) // D - точка для проверки
         {
             result = false;
-            if ((A.Y - B.Y) * D.X + (B.X - A.X) * D.Y + (A.X * B.Y - B.X * A.Y) < 0) // проверка лежит ли точка D за отрезком AB
+            if ((A.Y - B.Y) * D.X + (B.X - A.X) * D.Y + (A.X * B.Y - B.X * A.Y) <= 0) // проверка лежит ли точка D за отрезком AB
             {
-                if ((C.Y - B.Y) * D.X + (B.X - C.X) * D.Y + (C.X * B.Y - B.X * C.Y) < 0)  // проверка лежит ли точка D за отрезком BC
+                if ((B.Y - C.Y) * D.X + (C.X - B.X) * D.Y + (B.X * C.Y - C.X * B.Y) <= 0)  // проверка лежит ли точка D за отрезком BC
                 {
-                    if ((C.Y - A.Y) * D.X + (A.X - C.X) * D.Y + (C.X * A.Y - A.X * C.Y) < 0) // проверка лежит ли точка D за отрезком AC
+                    if ((C.Y - A.Y) * D.X + (A.X - C.X) * D.Y + (C.X * A.Y - A.X * C.Y) <= 0) // проверка лежит ли точка D за отрезком AC
                     {
                         result = true;
                     }
